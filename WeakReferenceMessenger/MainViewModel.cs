78e97494d084534414a89bb4e07e4736b00b5cad
Craig Colomb <craig.colomb@hsbcad.com>
2022-07-14 08:33:27 -0700

    Add project files.

---
+++ WeakReferenceMessenger/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+
+namespace WeakReferenceMessengerTest
+{
+    public partial class MainViewModel:ObservableObject
+    {
+        public MainViewModel()
+        {
+            NestedVM = new();
+            WeakReferenceMessenger.Default.Register<string>(this, (r, m) =>
+            {
+                ReceivedMessage = $"Received:{m}";
+            });
+        }
+
+        [ObservableProperty]
+        private string receivedMessage;
+
+        [ObservableProperty]
+        private NestedViewModel nestedVM;
+    }
+}
---
+++ WeakReferenceMessenger/NestedViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace WeakReferenceMessengerTest
+{
+    public partial class NestedViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        private string enteredMessage;
+
+        [RelayCommand]
+        void SendMessage()
+        {
+            WeakReferenceMessenger.Default.Send(enteredMessage);
+        }
+    }
+}
